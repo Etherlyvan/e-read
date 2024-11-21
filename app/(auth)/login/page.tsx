38a0e593aa37702409728144ed66c9891e0e6fd9
Etherlyvan <jovanku1@gmail.com>
2024-11-21 18:54:51 +0700

    RegisterLogin Push2

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,14 +6,14 @@ const Login = ({searchParams}:{searchParams?:{error?:string}}) => {
     return (
       <div className="p-6 space-y-4">
         <h1 className="text-2xl font-bold text-gray-900">Sign In to your Account</h1>
-        {params === "OAuthAccountNotLinked"? (
-          <div
+        {params === "OAuthAccountNotLinked" ? (
+        <div
           className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100"
           role="alert"
         >
           <span className="font-medium">Account Already Linked in Another Provider</span>
         </div>
-        ) : null}
+      ) : null}
         
 
         <FormLogin />
