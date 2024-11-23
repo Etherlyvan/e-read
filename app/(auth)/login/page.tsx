d315b5dca5f3d01fb1e26ad52301ef387a7b490b
Etherlyvan <jovanku1@gmail.com>
2024-11-23 10:44:06 +0700

    RegisterLogin Push4

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,37 +5,35 @@ interface SearchParams {
   error?: string;
 }
 
-interface PageProps {
+interface LoginProps {
   searchParams?: SearchParams;
 }
 
+const Login: React.FC<LoginProps> = ({ searchParams }) => {
+  const loginError = searchParams?.error;
 
-const Login: React.FC<PageProps> = ({ searchParams }) => {
-  const params = searchParams?.error;
-
-    return (
-      <div className="p-6 space-y-4">
-        <h1 className="text-2xl font-bold text-gray-900">Sign In to your Account</h1>
-        {params === "OAuthAccountNotLinked" ? (
-        <div
-          className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100"
-          role="alert"
-        >
-          <span className="font-medium">Account Already Linked in Another Provider</span>
-        </div>
-      ) : null}
-        
-
-        <FormLogin />
-          <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300">
-            <p className="mx-4 mb-0 text-center font-semibold text-gray-600">Or</p>
-          </div>
-
-        <GoogleButton />
-        <GithubButton />
+  return (
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold text-gray-900">Sign In to your Account</h1>
+      {loginError === "OAuthAccountNotLinked" ? (
+      <div
+        className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100"
+        role="alert"
+      >
+        <span className="font-medium">Account Already Linked in Another Provider</span>
       </div>
-    );
-  };
-  
-  export default Login;
-  
+    ) : null}
+      
+
+      <FormLogin />
+        <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300">
+          <p className="mx-4 mb-0 text-center font-semibold text-gray-600">Or</p>
+        </div>
+
+      <GoogleButton />
+      <GithubButton />
+    </div>
+  );
+};
+
+export default Login;
