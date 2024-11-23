8b4cf0ab5daed1b613ec1594a915bec5c5f6956d
Etherlyvan <jovanku1@gmail.com>
2024-11-23 12:53:15 +0700

    RegisterLogin PushCancel

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,36 +1,20 @@
-import { ReactNode } from "react";
+import FormLogin from "@/components/auth/form-login";
+import { GithubButton, GoogleButton } from "@/components/auth/social-button";
 
-interface SegmentParams {
-  id: string;
-}
-
-export interface LayoutProps {
-  children?: React.ReactNode;
-  params?: Promise<SegmentParams>;
-}
-
-export default async function Layout(props: { children: ReactNode; params: Promise<{ id: string }> }) {
-  const { children, params } = props;
-  const { id } = await params;
-
-  const renderErrorAlert = (error?: string) => {
-    if (error === "OAuthAccountNotLinked") {
-      return (
-        <div
-          className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100"
-          role="alert"
-        >
-          <span className="font-medium">Account Already Been Used in another Platform</span>
-        </div>
-      );
-    }
-    return null;
-  };
-
+const Login = () => {
   return (
-    <div>
-      {children}
-      {renderErrorAlert(id)}
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold text-gray-900">
+        Sign In to your account
+      </h1>
+      <FormLogin />
+      <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300">
+        <p className="mx-4 mb-0 text-center font-semibold text-gray-600">Or</p>
+      </div>
+      <GoogleButton />
+      <GithubButton />
     </div>
   );
-}
+};
+
+export default Login;
