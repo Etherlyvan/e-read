e7eb2f90ef903c4d95235232e6e25780b39b604d
Etherlyvan <jovanku1@gmail.com>
2024-11-25 14:49:44 +0700

    Card fix newBook

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,9 +16,12 @@ const Dashboard = async () => {
       <div className="max-w-screen-lg mx-auto py-14">
         <div className="flex items-end justify-between">
           <h1 className="text-4xl font-bold">Latest Book</h1>
-          <Link href="/create" className="py-3 px-6 bg-blue-600 hover:bg-blue-700 text-white">
+          {session && session.user.role === 'admin' && (
+            <Link href="/create" className="py-3 px-6 bg-blue-600 hover:bg-blue-700 text-white">
             New Book
-          </Link>
+            </Link>
+          )}
+          
         </div>
         <div className="grid md:grid-cols-4 gap-5 mt-10">
           {/* Content goes here */}
